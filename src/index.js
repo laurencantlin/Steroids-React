c42d12274b38b4a46c0e57c1b669a270d37edecf
laurencantlin <laurenrose5321@gmail.com>
2018-05-28 17:05:13 -0400

    working react code

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Office = window.Office;
+
+Office.initialize = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
